1661dd36744f0bcde45be84f1039deb5496be3b2
Chris Veleris <chris.veleris@me.com>
2015-06-05 22:42:18 +0300

    Added signout link to homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
+  
+  # devise filter to authenticate user before calling controller
+  before_filter :authenticate_user!
+  
   def index
   end
+  
 end
