de653a89879e3012372b6f6b65a312908b558c3a
Chris Veleris <chris.veleris@me.com>
2015-06-06 01:12:25 +0300

    Added file import action to index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,21 @@
+require 'csv'
+
 class HomeController < ApplicationController
   
   # devise filter to authenticate user before calling controller
   before_filter :authenticate_user!
   
   def index
+  end
+  
+  def import
+    fileText = File.read(params[:file].path)
+    fileData = CSV.parse(fileText, headers: false)
+    fileData.each do |row|
+      puts row
+    end
+
+    redirect_to root_path, notice: "Data imported."
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   
-  get 'home/index'
+  post 'import' => 'home#import'
 
   devise_for :users
   root to: "home#index"
