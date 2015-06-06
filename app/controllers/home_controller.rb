ba9a52d1b4a12ed2bc72beb7d4236212187ac293
Chris Veleris <chris.veleris@me.com>
2015-06-06 17:00:26 +0300

    Package name sliced with regex

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,12 +10,34 @@ class HomeController < ApplicationController
   
   def import_data
     @lines = Array.new
+    @package_info = Array.new
+    
     if !params[:file].nil?
       logfile_path = params[:file].path
       File.foreach(logfile_path, headers: false) do |row|
-        @lines << row.to_s.split.to_a
+        # split each row to whitespace
+        @logdata = row.to_s.split
+        # merge date values
+        @install_date = @logdata[0] + " " + @logdata[1] + " " + @logdata[2]
+        # strip the ":" from the end of the status
+        @status = @logdata[3].sub(/\:/, '')
+        # slice the package name
+        regex = /^[\d:]*((?:\w+[\w\d]*-)+)(\d+)\.?([\d\.]*)(?:-(\d+)\.(\w+)\.?(\w+)?)*/
+        if @logdata[4].to_s.match(regex)
+          @package_info =  @logdata[4].to_s.match(regex)
+        
+          @package_name = @package_info[1].gsub(/-/,' ')
+          @major_rel = @package_info[2]
+          @minor_rel = @package_info[3] 
+          @minor_rel += "-" + @package_info[4] if @package_info[4]
+          @platform = @package_info[6] if @package_info[6]
+
+          @lines << [@install_date, @status, @package_name, @major_rel, @minor_rel, @platform]
+        end
       end
     end
+    
+    
   end
   
 end
