d12363493cb81afa7470a0f4c472ca72522c2f63
Chris Veleris <chris.veleris@me.com>
2015-06-06 21:32:07 +0300

    Display grouped data completed

--- app/controllers/logevents_controller.rb
+++ app/controllers/logevents_controller.rb
@@ -4,6 +4,9 @@ class LogeventsController < ApplicationController
   
   def index
     @logevents = LogEvent.all
+    @logevents_datetime = @logevents.group_by(&:install_date)
+    @logevents_status = @logevents.group_by(&:status)
+    @logevents_pname = @logevents.group_by(&:package_name)
   end
 
   def new
