56f1c51827465b9745b72b4f2ec322ef962098f9
Chris Veleris <chris.veleris@me.com>
2015-06-07 12:35:55 +0300

    Added migration for logevents to logfile reference (small fix - removed id)

--- db/migrate/20150607092601_add_logfile_id_to_log_events.rb
+++ db/migrate/20150607092601_add_logfile_id_to_log_events.rb
@@ -1,11 +1,11 @@
 class AddLogfileIdToLogEvents < ActiveRecord::Migration
   
   def up
-    add_reference :log_events, :logfile_id, index: true
+    add_reference :log_events, :logfile, index: true
   end
   
   def down
-    remove_reference :log_events, :logfile_id
+    remove_reference :log_events, :logfile
   end
   
 end
